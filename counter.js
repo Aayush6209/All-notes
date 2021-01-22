c64959d65f700b29966cdfac4eb177572623ce9b
Aayush <aayushgupta6209@gmail.com>
2021-01-22 12:15:54 +0530

    setInterval function

--- counter.js
+++ counter.js
@@ -4,11 +4,14 @@ function count() {
     counter++;
     document.querySelector('h1').innerHTML = counter;
 
-    if (counter % 10 === 0) {
-        alert(`Count is multiple of ${counter}`)
-    }
+    // if (counter % 10 === 0) {
+    //     alert(`Count is multiple of ${counter}`)
+    // }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 100);
+
 })
