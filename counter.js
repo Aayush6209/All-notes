558a7419f7bc9f90abe6e0838ec04df49541f209
Aayush <aayushgupta6209@gmail.com>
2021-01-22 12:32:48 +0530

    storing/retrieving data in browser

--- counter.js
+++ counter.js
@@ -1,17 +1,20 @@
-let counter = 0;
+// if counter not in storage, then set it
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    // retrive
+    let counter = localStorage.getItem('counter');
+
     counter++;
     document.querySelector('h1').innerHTML = counter;
-
-    // if (counter % 10 === 0) {
-    //     alert(`Count is multiple of ${counter}`)
-    // }
+    // save
+    localStorage.setItem('counter', counter);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Just after loading html set count value to current value in storage
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
-
-    setInterval(count, 100);
-
 })
